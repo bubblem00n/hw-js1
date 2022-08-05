0197bbc40d8081d88b4e8fa5a251c60f406254cd
bubblem00n <107258006+bubblem00n@users.noreply.github.com>
2022-08-05 22:30:24 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let numberFirst = 14;
+let numberSecond = 7;
+
+let sum = numberFirst + numberSecond;
+let minus = numberFirst - numberSecond;
+let multiply = numberFirst * numberSecond;
+let devide = numberFirst / numberSecond;
+let total = sum + minus + multiply + devide;
+
+console.log("Сложение: " + "14 + 7 = " + sum);
+console.log("Вычитание: " + "14 - 7 = " + minus);
+console.log("Умножение: " + "14 * 7 = " + multiply);
+console.log("Деление: " + "14 / 7 = " + devide);
+console.log("Общее: " + "21 + 7 + 98 + 2 = " + total);
+
